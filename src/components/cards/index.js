ff9af5bf80e850ec1a0c4a2debe7233835132d2a
juancuervo01 <108945176+juancuervo01@users.noreply.github.com>
2023-05-21 22:54:19 -0500

    Eliminar modales

--- src/components/cards/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FoodMenuCard } from './FoodMenuCard'
-export { default as ReserveListCard } from './ReserveListCard'
-export { default as TicketListCard } from './TicketListCard'
--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,22 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
-// import { getAnalytics } from 'firebase/analytics'
-// import { getFirestore } from 'firebase/firestore'
+// import { getAnalytics } from 'firebase/analytics';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyCqlIKz1sjEpbNTYQz8HtEKXLuCo0M2IwQ',
-  authDomain: 'unirestaurant-fadcb.firebaseapp.com',
-  projectId: 'unirestaurant-fadcb',
-  storageBucket: 'unirestaurant-fadcb.appspot.com',
-  messagingSenderId: '1050000175006',
-  appId: '1:1050000175006:web:ffefc07f53a4c941f1708f',
-  measurementId: 'G-F9T16ZBSVS'
+  apiKey: 'AIzaSyC2BX-BLSbpEAOulHhFpekgwgTxCGcY1VU',
+  authDomain: 'firm-camp-340804.firebaseapp.com',
+  databaseURL: 'https://firm-camp-340804-default-rtdb.firebaseio.com',
+  projectId: 'firm-camp-340804',
+  storageBucket: 'firm-camp-340804.appspot.com',
+  messagingSenderId: '651438910839',
+  appId: '1:651438910839:web:c4f56f1e25a3d983010572',
+  measurementId: 'G-MX4B44DXP8'
 }
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
-// export const db = getFirestore(app)
-// export const analytics = getAnalytics(app)
+// const analytics = getAnalytics(app);
